2755ca7516e51f3e843124023492169f9e170ba9
Nahomy <diaznahomy341@gmail.com>
2025-12-09 10:34:05 -0600

    Refactor code structure for improved readability and maintainability

--- lab09/src/App.jsx
+++ lab09/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import adult from './assets/adult.png'
+import boy from './assets/boy.png'
+import Likebutton from './Likebutton'
 
 function App() {
   // Estado para el contador
   //Hook useState
-  const [count, setCount] = useState(0)
-
+  const [age, setAge] = useState(0)
+  const [isAdult, setIsAdult] = useState(false)
+  useEffect(() => {
+    setIsAdult(age >= 18)
+  }, [age])
   return (
     <>
       <div>
@@ -20,16 +26,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setAge((age) => age + 1)}>
+          count is {age}
         </button>
         <Likebutton></Likebutton>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div className = "avatars">
+      <img src={isAdult ? adult : boy} alt = "avatar"
+      />  
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+
       </p>
     </>
   )
